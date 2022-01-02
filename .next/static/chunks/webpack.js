ad57882cdf119240bca7041873d3a147828709ab
dtune83 <latoco12@gmail.com>
2022-01-01 23:14:49 -0700

    gitignore update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0018a98e22537e2f"; }
+/******/ 		__webpack_require__.h = function() { return "e6fc19cbebe0b92c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
